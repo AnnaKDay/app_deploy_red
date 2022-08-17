e9dd6fd3164ad75dd75611827d5d0a280f313709
AnnaKDay <katsukoday@gmail.com>
2022-08-16 20:10:11 -0500

    test again

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import psycopg2
 from flask import Flask, render_template, request, url_for, redirect
 
 from config import db_password
-key = os.environ['DB_PASSWORD']
 
 
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(database="N-Butane", user="postgres", password=key, host="127.0.0.1", port="5432")
+    conn = psycopg2.connect(database="N-Butane", user="postgres", password=db_password, host="127.0.0.1", port="5432")
     # will need to create a database, and make a config.py to hold the password 
     return conn
 
